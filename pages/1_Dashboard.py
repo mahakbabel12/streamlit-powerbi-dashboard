47d7c8972c9ff92e457bcaec88e258e673e4c881
mahak <mahakbabel26082003@mail.com>
2025-04-09 10:34:56 +0530

    added

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -4,7 +4,7 @@ from streamlit.components.v1 import html
 st.markdown("### 🔍 Live Power BI Dashboard")
 
 # Replace this with your actual Power BI embed URL
-powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=ec54d7ea-e794-4210-ae0c-5aa7b639da39&autoAuth=true&ctid=f8fdcadc-a212-41dd-b543-c6e2f23d970c"
+powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=0ca9fbde-75d5-4528-b332-0568ad5e2d15&autoAuth=true&ctid=f8fdcadc-a212-41dd-b543-c6e2f23d970c"
 
 html(
     f"""
